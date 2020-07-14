4c1e816827d8b125883276c61a16b9495a68f326
kartikeyam91 <66990280+kartikeyam91@users.noreply.github.com>
2020-07-14 23:27:56 +0530

    Update Modification 14 Jul 20.sql

--- Modification 14 Jul 20.sql
+++ Modification 14 Jul 20.sql
@@ -1,2 +1,6 @@
+--14 Jul 20---
+/*
+Run the following command of adding a column before executing spProdCategoryInsertUpdateDelet
+*/
 ALTER TABLE product_category
 ADD COLUMN prod_category_datetill TIMESTAMP DEFAULT NULL
