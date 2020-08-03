5e03726c34567469b963e991eaac952d80610d4d
kartikeyam91 <66990280+kartikeyam91@users.noreply.github.com>
2020-08-04 00:59:06 +0530

    table definition 04 Aug

--- Modification 14 Jul 20.sql
+++ Modification 14 Jul 20.sql
@@ -28,3 +28,29 @@ CREATE TABLE coupon(
 
 ALTER TABLE coupon
 ADD COLUMN coupon_datetill TIMESTAMP DEFAULT NULL
+
+----------------------04 Aug 20------------------
+-----Create table ref_size---------------Begin
+-- Table: public.ref_size
+
+-- DROP TABLE public.ref_size;
+
+CREATE TABLE public.ref_size
+(
+    size_code character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    size_value character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    prod_category_id integer NOT NULL,
+    size_id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    size_datetill timestamp without time zone,
+    CONSTRAINT ref_size_pkey PRIMARY KEY (size_id),
+    CONSTRAINT ref_size_prod_category_id_fkey FOREIGN KEY (prod_category_id)
+        REFERENCES public.product_category (prod_category_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.ref_size
+    OWNER to postgres;
+-----Create table ref_size---------------End
