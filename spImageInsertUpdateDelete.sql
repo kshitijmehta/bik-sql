22c20f5b66064053b0d7008a69187e6107503d8a
kartikeyam91 <66990280+kartikeyam91@users.noreply.github.com>
2020-09-27 16:11:52 +0530

    Update spImageInsertUpdateDelete.sql

--- spImageInsertUpdateDelete.sql
+++ spImageInsertUpdateDelete.sql
@@ -17,18 +17,39 @@ CREATE OR REPLACE PROCEDURE spImageInsertUpdateDelete(
 LANGUAGE 'plpgsql'
 
 AS $BODY$
+	DECLARE  
+	_sql TEXT := 'INSERT INTO product_image (prod_id, prod_img_name, prod_img_path) 
+	 		VALUES ($2,$3,$4)';
+	_val text;
+			
 
 BEGIN 
 
 	IF _ser = 1 THEN -- insert
-		INSERT INTO product_image (prod_id, prod_img_name, prod_img_path) 
-		VALUES (_prodid,_name1, _imgpath1),(_prodid, _name2, _imgpath2),(_prodid, _name3, _imgpath3),
-				(_prodid, _name4, _imgpath4),(_prodid, _name5, _imgpath5);
+-- 		INSERT INTO product_image (prod_id, prod_img_name, prod_img_path) 
+-- 		VALUES (_prodid,_name1, _imgpath1),(_prodid, _name2, _imgpath2),(_prodid, _name3, _imgpath3),
+-- 				(_prodid, _name4, _imgpath4),(_prodid, _name5, _imgpath5);
+		_val = CONCAT (
+				CASE WHEN _name2 IS NOT NULL THEN ',($2,$5,$6)' END,
+				CASE WHEN _name3 IS NOT NULL THEN ',($2,$7,$8)' END,
+				CASE WHEN _name4 IS NOT NULL THEN ',($2,$9,$10)' END,
+				CASE WHEN _name5 IS NOT NULL THEN ',($2,$11,$12)' END
+		);
+		
+		IF _val <> '' THEN
+			_sql := _sql || _val ||';';
+		ELSE
+			_sql := _sql || ';';
+		END IF;
+		
+		EXECUTE _sql
+		USING _ser,_prodid,_name1,_img1path,_name2,_img2path,
+			  _name3, _img3path, _name4, _img4path, _name5, _img5path;
 	
 	ELSEIF _ser = 2 THEN --update
 		UPDATE product_image set
 		 prod_img_name = _name1,
-		 prod_img_path = _imgpath1
+		 prod_img_path = _img1path
 		 where prod_img_id = _imgid;
 		 
 	ELSEIF _ser = 3 THEN --DELETE
