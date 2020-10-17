9c6f601a1d11cc6c6517e4efe0360aa56f4e21b6
kartikeyam91 <66990280+kartikeyam91@users.noreply.github.com>
2020-10-17 17:25:33 +0530

    Update fnproductlistselect.sql

--- fnproductlistselect.sql
+++ fnproductlistselect.sql
@@ -1,37 +1,50 @@
+-- FUNCTION: public.fnproductlistselect(text, text, text)
+
+-- DROP FUNCTION public.fnproductlistselect(text, text, text);
+--select * from fnproductlistselect (_colour:='(1,2)')
+--select * from fnproductlistselect (_size:='(1)',_colour:='(1,2)')
+
+
 CREATE OR REPLACE FUNCTION public.fnproductlistselect(
-	_colour text default NULL,
-	_size text DEFAULT NULL,
-	_price TEXT DEFAULT NULL)
+	_colour text DEFAULT NULL::text,
+	_size text DEFAULT NULL::text,
+	_price text DEFAULT NULL::text)
     RETURNS TABLE(prodid integer, prodcategory character varying, prodname character varying, proddesc text, inrprice numeric, usdprice numeric, colour character varying, size character varying, qty integer) 
     LANGUAGE 'plpgsql'
 
-       
+    COST 100
+    VOLATILE 
+    ROWS 1000
+    
 AS $BODY$
 	
 	DECLARE
 	
-	_sql TEXT := 'SELECT b.prod_id,c.prod_category, a.prod_name,a.prod_desc, b.prod_inr_price,
-		b.prod_usd_price, d.colour_value, e.size_value,b.prod_qty FROM product_sub_category a
+	_sql TEXT := 'SELECT b.prod_id,c.prod_category, b.prod_name,b.prod_desc, pd.prod_inr_price,
+		pd.prod_usd_price, d.colour_value, e.size_value,pd.prod_qty FROM product_sub_category a
 		INNER JOIN product b ON a.prod_subcateg_id = b.prod_subcateg_id
 		INNER JOIN product_category c ON a.prod_category_id = c.prod_category_id
-		LEFT JOIN ref_colour d ON d.colour_id = b.prod_colour
-		LEFT JOIN ref_size e ON e.size_id = b.prod_size ';
-	--	WHERE 1=1 ';
+		INNER JOIN product_details pd ON b.prod_id = pd.prod_id
+		LEFT JOIN ref_colour d ON d.colour_id = pd.prod_colour
+		LEFT JOIN ref_size e ON e.size_id = pd.prod_size '; 
+		--WHERE 1=1 ';
 	_where TEXT;
-	
+
 	BEGIN
 		 
 		 _where = CONCAT_WS(' AND '
-						   ,  CASE WHEN _colour IS NOT NULL THEN '('||_colour||')' END
-						   ,  CASE WHEN _size IS NOT NULL THEN _size END
-						   ,  CASE WHEN _price IS NOT NULL THEN _price END);
+						  -- ,  CASE WHEN _colour IS NOT NULL THEN '('||_colour||')' END
+						   ,  CASE WHEN _colour IS NOT NULL THEN 'd.colour_id in ' ||_colour||' ' END
+						   ,  CASE WHEN _size IS NOT NULL THEN 'e.size_id in ' || _size||' ' END
+						   ,  CASE WHEN _price IS NOT NULL THEN _price END||' ');
 		
 		 IF _where <> '' THEN
-		 	_sql :=_sql ||'where '|| _where ||'order by b.prod_id';
+ 		 	_sql :=_sql ||' where '|| _where ||'order by b.prod_id';
+			--_sql :=_sql || _where ||' order by b.prod_id';
 		
 		 ELSE
 		  
-		 	_sql := _sql ||'order by b.prod_id';
+		 	_sql := _sql ||' order by b.prod_id';
 		
 		END IF;
 			 RETURN QUERY
@@ -41,4 +54,5 @@ AS $BODY$
 		END
 $BODY$;
 
-
+ALTER FUNCTION public.fnproductlistselect(text, text, text)
+    OWNER TO postgres;
