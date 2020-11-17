762f3ab6574ab9279e25a765820a1038c77d7bb8
kartikeyam91 <66990280+kartikeyam91@users.noreply.github.com>
2020-11-17 23:09:18 +0530

    Prices added

--- Functions/fnGetLatestTrendingProduct.sql
+++ Functions/fnGetLatestTrendingProduct.sql
@@ -1,19 +1,32 @@
-CREATE OR REPLACE FUNCTION fnGetLatestTrendingProduct ( 
-	i INT)
-RETURNS TABLE(prod_id integer, prod_name character varying, prod_desc text, prod_img_name character varying, prod_img_path text) 
-LANGUAGE 'plpgsql'
-AS
-$BODY$
+-- FUNCTION: public.fngetlatesttrendingproduct(integer)
+
+-- DROP FUNCTION public.fngetlatesttrendingproduct(integer);
+
+CREATE OR REPLACE FUNCTION public.fngetlatesttrendingproduct(
+	i integer)
+    RETURNS TABLE(prod_id integer, prod_name character varying, prod_desc text, 
+				  prod_inr_price numeric, prod_usd_price numeric,prod_img_name character varying, prod_img_path text) 
+    LANGUAGE 'plpgsql'
+
+    COST 100
+    VOLATILE 
+    ROWS 1000
+    
+AS $BODY$
 	BEGIN
 		IF i = 1 THEN ---- LATEST
 			RETURN QUERY
 			
 			select a.prod_id,a.prod_name,
-				a.prod_desc, b.prod_img_name, b.prod_img_path from public.product a
+				a.prod_desc
+				,pd.prod_inr_price, pd.prod_usd_price,
+				b.prod_img_name, b.prod_img_path from public.product a
 			left join public.product_image b on a.prod_id=b.prod_id 
+			left join public.product_details pd on a.prod_id = pd.prod_id
 			where a.prod_id in (select pd.prod_id from product_details pd)
 			and a.prod_datetill is null
 			and a.prod_latest = true 
+			and pd.pd_id = (select pds.pd_id from public.product_details pds where pds.prod_id = a.prod_id LIMIT 1)
 			and b.prod_img_name = (select pi.prod_img_name from public.product_image pi where pi.prod_id=a.prod_id LIMIT 1);
 		
 		ELSEIF i = 2 THEN ---- TRENDING
@@ -21,11 +34,15 @@ $BODY$
 			RETURN QUERY
 			
 			select a.prod_id,a.prod_name,
-				a.prod_desc, b.prod_img_name, b.prod_img_path from public.product a
+				a.prod_desc
+				,pd.prod_inr_price, pd.prod_usd_price
+				, b.prod_img_name, b.prod_img_path from public.product a
 			left join public.product_image b on a.prod_id=b.prod_id 
+			left join public.product_details pd on a.prod_id = pd.prod_id
 			where a.prod_id in (select pd.prod_id from product_details pd)
 			and a.prod_datetill is null
 			and a.prod_trending = true
+			and pd.pd_id = (select pds.pd_id from public.product_details pds where pds.prod_id = a.prod_id LIMIT 1)
 			and b.prod_img_name = (select pi.prod_img_name from public.product_image pi where pi.prod_id=a.prod_id LIMIT 1);
 		
 		END IF;
@@ -33,3 +50,6 @@ $BODY$
 	END;
 
 $BODY$;
+
+ALTER FUNCTION public.fngetlatesttrendingproduct(integer)
+    OWNER TO postgres;
