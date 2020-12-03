a7a7aa4db7b15eafbbe73cd73ccd77d7ddf6ccc6
kartikeyam91 <66990280+kartikeyam91@users.noreply.github.com>
2020-12-04 00:44:59 +0530

    Wedges 2nd Import

--- Data Import.sql
+++ Data Import.sql
@@ -436,3 +436,30 @@ INSERT INTO product_details (prod_id,prod_inr_price,prod_usd_price,prod_size,pro
 	where sd.dateinsert ='2020-12-03 20:12:50.831904'
 	order by pr.prod_id,rs.size_id
 --------------Bellies,wedges,flats_bellies_csv----End-----------
+-------------Bellies,wedges,flats_wedges_csv----Begin-----------
+COPY public.sampledata_wedges(stylecode,description,sku,mrp,usd,size,qty,colour,productname,model,model2,model3,model4,model5) 
+	FROM 'F:\KP\SB\Data\Footwear\Bellies,wedges,flats_wedges_csv.csv' DELIMITER ',' CSV HEADER;
+UPDATE sampledata_wedges set dateinsert = now() where dateinsert is null --- Use the latest date for items imported in this iteration
+-----------------------
+select distinct(dateinsert) from sampledata_wedges
+INSERT INTO ref_size (size_value,size_code, prod_category_id) --- 0 Rows
+	select distinct(size),size,1 from sampledata_wedges where dateinsert ='2020-12-04 00:33:45.295146' and size not in 
+	(select size_value from ref_size where prod_category_id=1)---- Use prod_category_id as per your db
+-------Insert Colour-----
+INSERT INTO ref_colour (colour_value,colour_code)  ---1 Row
+	select distinct(colour),substring(trim(colour),0,4) from sampledata_wedges where dateinsert ='2020-12-04 00:33:45.295146' and 
+	colour not in (select colour_value from ref_colour)
+-------Insert Products----
+INSERT INTO product (prod_stylecode,prod_name,prod_desc,prod_datetimeinserted,prod_subcateg_id) --19 Rows
+	select distinct on (stylecode) stylecode, productname, description,now()::timestamp,4 from sampledata_wedges
+	where dateinsert ='2020-12-04 00:33:45.295146'--- Add Prod Subcateg id as per ypur db
+	order by stylecode;
+	
+INSERT INTO product_details (prod_id,prod_inr_price,prod_usd_price,prod_size,prod_colour,prod_qty) --114 Rows
+	select  pr.prod_id,sd.mrp, sd.usd, rs.size_id, rc.colour_id, sd.qty from sampledata_wedges sd 
+	inner join product pr on sd.stylecode = pr.prod_stylecode
+	inner join ref_size rs on sd.size = rs.size_value
+	inner join ref_colour rc on sd.colour= rc.colour_value
+	where sd.dateinsert ='2020-12-04 00:33:45.295146'
+	order by pr.prod_id,rs.size_id
+-------------Bellies,wedges,flats_wedges_csv----End-----------	
