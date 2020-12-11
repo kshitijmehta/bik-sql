3eea28477053781aafaa496e43e6f4e154f978d1
kartikeyam91 <66990280+kartikeyam91@users.noreply.github.com>
2020-12-11 23:35:02 +0530

    flats import

--- Data Import.sql
+++ Data Import.sql
@@ -641,3 +641,38 @@ INSERT INTO product_details (prod_id,prod_inr_price,prod_usd_price,prod_size,pro
 	where sd.dateinsert = '2020-12-11 18:13:50.183101'
 	order by pr.prod_id,rs.size_id;
 ------------Bellies,heels,flats-Heels----------End-----------------
+------------Bellies,heels,flats-Final-Flats-------Begin
+COPY public.sampledata_flats(stylecode,productname,description,sku,mrp,usd,qty,colour,size,model,model2,model3,model4,model5) 
+	FROM 'F:\KP\SB\Data\Footwear\Bellies,heels,flats-Final-Flats_csv.csv' DELIMITER ',' CSV HEADER;---751 rows
+UPDATE sampledata_flats set dateinsert = now() where dateinsert is null --- Use the latest date for items imported in this iteration
+select distinct(dateinsert) from sampledata_flats----"2020-12-11 18:37:22.958961"
+-----------------------
+select distinct(dateinsert) from sampledata_flats
+INSERT INTO ref_size (size_value,size_code, prod_category_id) --- 0 Rows
+	select distinct(size),size,1 from sampledata_flats where dateinsert ='2020-12-11 18:37:22.958961' and size not in 
+	(select size_value from ref_size where prod_category_id=1)---- Use prod_category_id as per your db
+-------Insert Colour-----
+INSERT INTO ref_colour (colour_value,colour_code)  ---1 Row
+	select distinct(trim(colour)),substring(trim(colour),0,4) from sampledata_flats where dateinsert ='2020-12-11 18:37:22.958961' and 
+	trim(colour) not in (select colour_value from ref_colour)
+-------Insert Products----
+select * from product_sub_category---flats=2
+INSERT INTO product (prod_stylecode,prod_name,prod_desc,prod_datetimeinserted,prod_subcateg_id) --110 Rows
+	select distinct on (stylecode) stylecode, productname, description,now()::timestamp,2 from sampledata_flats
+	where dateinsert ='2020-12-11 18:37:22.958961'--- Add Prod Subcateg id as per ypur db
+	order by stylecode;
+	
+update sampledata_flats set stylecode='802_blue_2' where productname='Women Flat Sandals' and stylecode='802_blue'
+
+INSERT INTO product (prod_stylecode,prod_name,prod_desc,prod_datetimeinserted,prod_subcateg_id)
+	select distinct(stylecode), productname, description,now()::timestamp,2 from sampledata_flats where 
+	productname='Women Flat Sandals' and stylecode='802_blue_2';
+	
+INSERT INTO product_details (prod_id,prod_inr_price,prod_usd_price,prod_size,prod_colour,prod_qty) -- 751rows
+	select  pr.prod_id,sd.mrp, sd.usd, rs.size_id, rc.colour_id, sd.qty from sampledata_flats sd 
+	inner join product pr on sd.stylecode = pr.prod_stylecode
+	inner join ref_size rs on sd.size = rs.size_value
+	inner join ref_colour rc on sd.colour= rc.colour_value
+	where sd.dateinsert ='2020-12-11 18:37:22.958961'
+	order by pr.prod_id,rs.size_id
+------------Bellies,heels,flats-Final-Flats-------End
